9d9e62fe5364b81226a02ec5539393da893ba073
Andrey Grytsenko <andreyapg@gmail.com>
2022-11-13 08:57:06 +0200

    Retrieve Models by Hashids 2022-11-13

--- app/Support/Traits/HasHashid.php
+++ app/Support/Traits/HasHashid.php
@@ -4,16 +4,40 @@ declare(strict_types=1);
 namespace App\Support\Traits;
 
 use App\Support\Facades\Hashid;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Str;
 
 trait HasHashid
 {
-    public function getHashidAttribute()
+    /**
+     * @return string
+     */
+    public function getHashidAttribute(): string
     {
         return static::HASHID_PREFIX.$this->hashid_without_prefix;
     }
 
-    public function getHashidWithoutPrefixAttribute()
+    /**
+     * @return mixed
+     */
+    public function getHashidWithoutPrefixAttribute(): mixed
     {
         return Hashid::encode($this->id);
+    }
+
+    /**
+     * @param $hid
+     * @return mixed
+     */
+    public static function fidnOrFailByHashid($hid): mixed
+    {
+        $hash = Str::after($hid, static::HASHID_PREFIX);
+        $ids = Hashid::decode($hash);
+
+        if (empty($ids)) {
+            throw new ModelNotFoundException();
+        }
+
+        return static::findOrFail($ids[0]);
     }
 }

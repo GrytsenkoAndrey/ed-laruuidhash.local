98201213a5f31e56db83676cefd682d6a0aca67b
Andrey Grytsenko <andreyapg@gmail.com>
2022-11-13 09:02:14 +0200

    Routing resolution 2022-11-13

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Hashids\Hashids;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Route::bind('user_hashid', function ($value) {
+            return User::findOrFailByHashid($value);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// example.com/user/user_DngB0NV05ev1
+Route::get('/user/{user_hashid}', fn(User $user) => $user);
